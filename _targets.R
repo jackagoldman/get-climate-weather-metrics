b8db6be3b3fa0e6abc330b5f7ac5aec9af9a0dfc
jackagoldman <jandrewgoldman@gmail.com>
2024-03-14 20:38:55 -0400

    update pipeline

--- _targets.R
+++ _targets.R
@@ -10,8 +10,25 @@ tar_option_set(
 tar_source("src/get-era5-indices.R")
 tar_source("src/weather-analysis-tools.R")
 
+#paths
+matchedPairsDefol <- ""
+matchedPairsNonDefol <- 
+defolInfo <- 
+RES_DIR <- ""
+
+
+# set bandlist (metrics to extract from era5)
+bandList <- list('temperature_2m', 'total_precipitation_sum')
 
 # Replace the target list below with your own:
 list(
-  tar_target()
+  tar_target(matchedDefolFile, matchedPairsDefol, format = "file"), 
+  tar_target(matchedNonDefolFile, matchedPairsNonDefol, format = "file"), 
+  tar_target(defolShp, st_read()),
+  tar_target(NondefolShp, st_read()),
+  tar_target(defolInfoFile, defolInfo, format = "File"),
+  tar_target(defolInfoTable, read_csv(defolInfoFile)),
+  tar_target(preppedData, cleanDataDefol(defolShp, NondefolShp, defolInfoTable)),
+  tar_map()
+  
 )
