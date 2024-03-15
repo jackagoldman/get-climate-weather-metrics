713d10bf40ecf0af5ec369ec0858a9a877810754
jackagoldman <jandrewgoldman@gmail.com>
2024-03-15 14:21:18 -0400

    updated scripts

--- _get-weather-climate.R
+++ _get-weather-climate.R
@@ -11,13 +11,15 @@ tar_source("src/get-era5-indices.R")
 tar_source("src/weather-analysis-tools.R")
 
 #paths
-RES_DIR <- ""
-path2ConfigFile <-    #example "~/Code/python-rgee-config.py"
+RES_DIR <- "results/"
+path2ConfigFile <-  "~/Code/python-rgee-config.py"  #example "~/Code/python-rgee-config.py"
+filename <- "on-qc-wx-clim"
+extension <- ".shp"
 
 # point to config file
 reticulate::py_run_file(path2ConfigFile)
 # Initalize gee
-ee_Initialize()
+rgee::ee_Initialize()
 
 
 # set bandlist (metrics to extract from era5)
@@ -25,15 +27,19 @@ bandList <- list('temperature_2m', 'total_precipitation_sum')
 
 
 #get preprocessed data
-processed_data <- tar_read(preppredData, store = "store_preprocessing")
+processed_data <- tar_read(preppedData, store = "store_preprocessing")
 
 #set values to map over
 values_df <- tibble::tibble(processed_data) |> dplyr::select(c(id, prov))
 
 # Replace the target list below with your own:
-list(
-  tarchetypes::tar_map(
+
+list(tarchetypes::tar_map(
     values = values_df,
-    targets::tar_target(wx, weatherIndices(processed_data, bandList)))
-  
+    targets::tar_target(wx, weatherIndices(processed_data, bandList))),
+    targets::tar_target(results, command = dplyr::bind_rows(wx)),
+    targets::tar_target(write.results, output_wx(results, RES_DIR, filename, extension))
+
 )
+
+
--- _preprocessing.R
+++ _preprocessing.R
@@ -11,16 +11,16 @@ tar_source("src/get-era5-indices.R")
 tar_source("src/weather-analysis-tools.R")
 
 #paths
-matchedPairsDefol <- "/Users/jgoldman/Library/CloudStorage/OneDrive-UniversityofToronto/Data/qc-data/matching-pairs-design/clean-tables/"
-matchedPairsNonDefol <- 
-defolInfo <- 
+matchedPairsDefol <- "~/Desktop/OneDrive - University of Toronto/Data/chapter_3/on-qc-defol.shp"
+matchedPairsNonDefol <- "~/Desktop/OneDrive - University of Toronto/Data/chapter_3/on-qc-nondefol.shp"
+defolInfo <- "~/Work/PhD/sbw-fire-interactions/nbr-recovery/data/paired_fires/on-qc-defol-table.csv"
 
 # Replace the target list below with your own:
 list(
   tar_target(matchedDefolFile, matchedPairsDefol, format = "file"), 
   tar_target(matchedNonDefolFile, matchedPairsNonDefol, format = "file"), 
-  tar_target(defolShp, sf::st_read(matchedPairesDefol)),
-  tar_target(NondefolShp, sf::st_read(matchedPairedNondefol)),
+  tar_target(defolShp, sf::st_read(matchedDefolFile)),
+  tar_target(NondefolShp, sf::st_read(matchedNonDefolFile)),
   tar_target(defolInfoFile, defolInfo, format = "file"),
   tar_target(defolInfoTable, readr::read_csv(defolInfoFile)),
   tar_target(preppedData, cleanDataDefol(defolShp, NondefolShp, defolInfoTable))
